daab2be75d384a73c78d0f278b926d1f40a26c39
priyankr1 <160168368+priyankr1@users.noreply.github.com>
2025-01-31 12:40:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // This is the default behavior for most deployments
+});
+
